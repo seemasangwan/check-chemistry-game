b9510fded7dc739ec6ae7f87756fa8afefc8a97d
Seema <104920728+seemasangwan@users.noreply.github.com>
2023-12-14 12:53:59 +0530

    Update index.js
    In this I use createelement method and add css to that element in js file

--- index.js
+++ index.js
@@ -35,6 +35,17 @@ document.getElementById("chemistry").addEventListener("click",function()
 const myname=document.getElementById("myname").value;
 const othername=document.getElementById("othername").value;
 let res=check(myname,othername);
-document.getElementById("result").textContent=`${res}`;
+const result=document.createElement('h1');
+result.style.width='30%';
+result.style.border="10px solid red";
+result.style.marginLeft='35%';
+result.style.marginRight='35%';
+result.style.marginTop="20px";
+result.style.padding="20px";
+result.style.fontfamily="cursive";
+result.style.borderRadius="20px";
+result.style.textAlign="center";
+result.innerHTML=`${res}`;
+document.body.appendChild(result);
 
 });
